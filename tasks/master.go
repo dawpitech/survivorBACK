bec6ee3f06ce97b2d6c07db780e63b9901a1fc38
Antoine <me@antoinebellanger.fr>
2025-09-03 17:23:11 +0200

    refacto: Tasks master

--- tasks/master.go
+++ tasks/master.go
@@ -5,16 +5,33 @@ import (
 	"time"
 )
 
+type backgroundTasks struct {
+	Handler func()
+	Delay   time.Duration
+}
+
+var tasks = []backgroundTasks{
+	{
+		Handler: SyncUUIDs,
+		Delay:   time.Second * 10,
+	},
+	{
+		Handler: fetcher.UpdateData,
+		Delay:   time.Second * 30,
+	},
+}
+
 func RunTasksInBackground() {
-	ticker := time.NewTicker(time.Second * 10)
-	go func() {
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				SyncUUIDs()
-				fetcher.UpdateData()
+	for _, task := range tasks {
+		ticker := time.NewTicker(task.Delay)
+		go func() {
+			defer ticker.Stop()
+			for {
+				select {
+				case <-ticker.C:
+					task.Handler()
+				}
 			}
-		}
-	}()
+		}()
+	}
 }
