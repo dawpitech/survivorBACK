cd7910c56f8138d33b859f9a1a958ab10ea28aad
Antoine <me@antoinebellanger.fr>
2025-09-04 10:57:03 +0200

    wip(router): CORS policies

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func init() {
@@ -30,14 +29,7 @@ func main() {
 		return
 	}
 
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	router.Use(cors.Default())
 
 	defaultRoutes := router.Group("/api")
 	legacyRoutes := defaultRoutes.Group("/legacy/")
