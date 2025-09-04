566fcd0ace6bd5dd46c513e2c6fdc25b0616e1be
Antoine <me@antoinebellanger.fr>
2025-09-04 10:59:24 +0200

    feat(tasks): setting on/off for individual task

--- tasks/master.go
+++ tasks/master.go
@@ -6,16 +6,19 @@ import (
 )
 
 type backgroundTasks struct {
+	Enable  bool
 	Handler func()
 	Delay   time.Duration
 }
 
 var tasks = []backgroundTasks{
 	{
+		Enable:  true,
 		Handler: SyncUUIDs,
 		Delay:   time.Second * 10,
 	},
 	{
+		Enable:  true,
 		Handler: fetcher.UpdateData,
 		Delay:   time.Second * 30,
 	},
@@ -23,6 +26,9 @@ var tasks = []backgroundTasks{
 
 func RunTasksInBackground() {
 	for _, task := range tasks {
+		if !task.Enable {
+			continue
+		}
 		ticker := time.NewTicker(task.Delay)
 		go func() {
 			defer ticker.Stop()
