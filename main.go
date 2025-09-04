c75f2d416cdd3bd750e4d108656e645a4b1ae92e
Antoine <me@antoinebellanger.fr>
2025-09-04 10:13:16 +0200

    fix(router): allow local connections by CORS policies

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"FranceDeveloppe/JEB-backend/middlewares"
 	"FranceDeveloppe/JEB-backend/tasks"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func init() {
@@ -27,6 +29,15 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	defaultRoutes := router.Group("/api")
 	legacyRoutes := defaultRoutes.Group("/legacy/")
