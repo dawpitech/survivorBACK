4275d222dad1424be89c52c7dfd5c0457aa1df80
Antoine <me@antoinebellanger.fr>
2025-09-05 10:21:51 +0200

    fix: allow frontend dev server in CORS policies

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	}
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:80", "http://localhost", "http://localhost:24680"},
+		AllowOrigins:     []string{"http://localhost:80", "http://localhost", "http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
