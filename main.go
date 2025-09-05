9c071ffb787d638a987a8bb98f8b295969747ea4
Antoine <me@antoinebellanger.fr>
2025-09-05 12:04:43 +0200

    fix: allow PATCH requests in CORS policies

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:80", "http://localhost", "http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
